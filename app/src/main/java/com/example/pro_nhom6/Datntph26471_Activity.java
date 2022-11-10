007b6be445d6c38f01c98ee4218708082a53b61a
tienDat203 <datntph26471@fpt.edu.vn>
2022-11-10 20:58:18 +0700

    TienDat commit lan 3

--- app/src/main/java/com/example/pro_nhom6/Datntph26471_Activity.java
+++ app/src/main/java/com/example/pro_nhom6/Datntph26471_Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TienDat_Activity extends AppCompatActivity {
+public class Datntph26471_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tien_dat);
+        setContentView(R.layout.activity_datntph26471);
     }
 }
