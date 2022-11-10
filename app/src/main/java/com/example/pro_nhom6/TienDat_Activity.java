ae83f0f1b87371fb549a00791a89b113fef044a6
tienDat203 <datntph26471@fpt.edu.vn>
2022-11-10 20:52:29 +0700

    TienDat commit lan 2

---
+++ app/src/main/java/com/example/pro_nhom6/TienDat_Activity.java
@@ -0,0 +1,14 @@
+package com.example.pro_nhom6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TienDat_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tien_dat);
+    }
+}
